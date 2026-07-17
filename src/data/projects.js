792f7b4893e30e83d0bf16d62e2a52a642ce4214
Mohammed Ismail Vajeehullah <vajeehvjk@gmail.com>
2026-07-18 00:44:07 +0530

    08: Projects Links and Responsive Update

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,6 +11,21 @@ import accountbook from "../assets/projects/Software/AccountBookUI/accountbook-c
 import weatherapp from "../assets/projects/Software/WeatherApp/weatherapp-cover-img.png";
 import sorc from "../assets/projects/Software/SROC/sorc-cover-img.png";
 import didak from "../assets/projects/Software/DidakPOS/didak-cover-img.png";
+import vjcafe from "../assets/projects/Software/VJCafe/vj-cafe-cover-img.png";
+
+/* =========================
+   UI / UX Projects
+========================= */
+
+import eventManage from "../assets/projects/UIUX/EventManage/event-cover-img.png";
+import nmg from "../assets/projects/UIUX/NMG/nmg-cover-img.png";
+import travelAgency from "../assets/projects/UIUX/TravelAgencyUIUX/travelagency-cover-img.png";
+
+/* =========================
+   Graphic Design Projects
+========================= */
+
+import techcareFolio from "../assets/projects/Graphics/TechcareFolio/techcarefolio-cover-img.jpeg";
 
 const projects = [
 
@@ -19,7 +34,10 @@ const projects = [
         category: "Software",
         title: "Tech Care E-Commerce",
         slug: "tech-care",
-        cover: techcare
+        cover: techcare,
+        previewType: "website",
+        previewLink: "https://shop.techcare.qa/",
+        responsive: true
     },
 
     {
@@ -27,15 +45,10 @@ const projects = [
         category: "Software",
         title: "Personal Website Template 01",
         slug: "personal-website-template-01",
-        cover: personal01
-    },
-
-    {
-        id: 3,
-        category: "Software",
-        title: "Personal Website Template 02",
-        slug: "personal-website-template-02",
-        cover: personal02
+        cover: personal01,
+        previewType: "website",
+        previewLink: "https://yasthan.netlify.app/",
+        responsive: true
     },
 
     {
@@ -43,15 +56,32 @@ const projects = [
         category: "Software",
         title: "CodeNest",
         slug: "codenest",
-        cover: codenest
+        cover: codenest,
+        previewType: "website",
+        previewLink: "https://codenestsite.netlify.app/",
+        responsive: true
     },
 
     {
         id: 5,
         category: "Software",
-        title: "Tic Tac Toe",
-        slug: "tic-tac-toe",
-        cover: tictactoe
+        title: "X or O Game",
+        slug: "x-or-o-game",
+        cover: tictactoe,
+        previewType: "website",
+        previewLink: "https://x-or-o-game.netlify.app/",
+        responsive: true
+    },
+
+    {
+        id: 3,
+        category: "Software",
+        title: "Personal Website Template 02",
+        slug: "personal-website-template-02",
+        cover: personal02,
+        previewType: "website",
+        previewLink: "https://wajeebullah-portfolio.netlify.app/",
+        responsive: false
     },
 
     {
@@ -59,7 +89,10 @@ const projects = [
         category: "Software",
         title: "Account Book UI",
         slug: "account-book-ui",
-        cover: accountbook
+        cover: accountbook,
+        previewType: "website",
+        previewLink: "https://accountbookmrzdeals.netlify.app/",
+        responsive: false     
     },
 
     {
@@ -67,7 +100,10 @@ const projects = [
         category: "Software",
         title: "Weather App",
         slug: "weather-app",
-        cover: weatherapp
+        cover: weatherapp,
+        previewType: "website",
+        previewLink: "https://weathercondtionchecker.netlify.app/",
+        responsive: true    
     },
 
     {
@@ -75,7 +111,10 @@ const projects = [
         category: "Software",
         title: "SORC",
         slug: "sorc",
-        cover: sorc
+        cover: sorc,
+        previewType: "github",
+        previewLink: "https://github.com/vajeeismail/SORC",
+        responsive: true
     },
 
     {
@@ -83,9 +122,77 @@ const projects = [
         category: "Software",
         title: "Didak POS",
         slug: "didak-pos",
-        cover: didak
-    }
+        cover: didak,
+        previewType: "website",
+        previewLink: "",
+        responsive: false
+    },
+
+    {
+        id: 10,
+        category: "Software",
+        title: "VJ Cofe & Rooms",
+        slug: "vj-cofe-rooms",
+        cover: vjcafe,
+        previewType: "website",
+        previewLink: "https://vjcoffeecafe.netlify.app/",
+        responsive: false
+    },
+    
+    /* =========================
+        UI / UX Projects
+    ========================= */
+
+    {
+        id: 11,
+        category: "UIUX",
+        title: "NGM Automobiles UI",
+        slug: "ngm-automobiles-ui",
+        cover: nmg,
+        previewType: "figma",
+        previewLink: "https://www.figma.com/proto/GPh17oFmlxPwIGyAnnR6q9/NGM?node-id=4-5",
+        responsive: true
+    },
+    
+    {
+        id: 12,
+        category: "UIUX",
+        title: "Event Management UI",
+        slug: "event-management-ui",
+        cover: eventManage,
+        previewType: "figma",
+        previewLink: "https://www.figma.com/proto/rwDiys3MLOLZB73RsgcLLK/Event-Book?node-id=14-974&starting-point-node-id=14%3A720",
+        responsive: true
+    },
+
+    {
+        id: 13,
+        category: "UIUX",
+        title: "Travel Agency UI / UX",
+        slug: "travel-agency-uiux",
+        cover: travelAgency,
+        previewType: "behance",
+        previewLink: "https://www.behance.net/gallery/243834179/UIUX-DESIGN-TRAVEL-AGENCY",
+        responsive: true
+    },
+
+    /* =========================
+    Graphic Design Projects
+    ========================= */
+
+    {
+        id: 14,
+        category: "Graphics",
+        title: "Tech Care Company Profile",
+        slug: "tech-care-company-profile",
+        cover: techcareFolio,
+        previewType: "behance",
+        previewLink: "https://www.behance.net/gallery/251814923/PORTFOLIO-TECH-CARE-(DESIGN-CONTENT-CREATION)",
+        responsive: true
+    } 
 
 ];
+
+
 
 export default projects;
